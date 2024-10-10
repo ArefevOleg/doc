9f4232731b0b5dbcd4e4e57aa22ab1b573d68672
Arefevpro <arefevmsk@gmail.com>
2024-10-10 20:52:15 +0300

    commit 6

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from '../styles/nav.module.css';
 import data from "../data/data.json"
-import styles from"../styles/nav.module.css"
 import { v1 } from 'uuid'
 
 const idItem = v1()
-console.log(idItem)
+
 
 export const Navigation = () => {
+    const [isSubmenuVisible, setIsSubmenuVisible] = useState(null);
+
+    const toggleSubmenu = (index: any) => {
+        setIsSubmenuVisible(isSubmenuVisible === index ? null : index);
+    };
+
+    const renderSubmenu = (index: any) => {
+        return (
+            <ul className={styles.submenu}>
+                {data.navigationJs.map((el, subIndex) => (
+                    <li key={subIndex}>
+                        <a href={el.href}>{el.title}</a>
+                    </li>
+                ))}
+            </ul>
+        );
+    };
+
     return (
-        <ul className={styles.wrapper}>
-            <h3>{data.navigationTitle[0].navTitle}</h3>
-            {data.navigationJs.map(el => {
-                return (
-                    <li key={idItem}><a href={el.href}>{el.title}</a></li>
-                )
-            })}
+        <ul className={styles.menu}>
+            {data.navigationTitle.map((title, index) => (
+                <li key={index}>
+                    <a href="#" onClick={() => toggleSubmenu(index)}>
+                        {title.navTitle}
+                    </a>
+                    {isSubmenuVisible === index && renderSubmenu(index)}
+                </li>
+            ))}
         </ul>
     );
 };
--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -3,10 +3,17 @@ import data from "../data/data.json"
 import styles from "../styles/page.module.css"
 
 export const Page = () => {
-    return (
-        <div>
+    const { mainSections } = data;
 
-        </div>
+    return (
+            <main id="main-doc" className={styles.main}>
+                {mainSections.map((section) => (
+                    <section key={section.id} className={styles.mainSection} id={section.id}>
+                        <header className={styles.title}>{section.title}</header>
+                        <div dangerouslySetInnerHTML={{ __html: section.content }} />
+                    </section>
+                ))}
+            </main>
     );
 };
 
